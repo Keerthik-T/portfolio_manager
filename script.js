c422dc3230684376326433cf1770896c3a360e2c
KEERTHIK T <140291659+Keerthik-T@users.noreply.github.com>
2026-04-26 22:00:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Fetch model info on load
-fetch(`${API_URL}/api/health`)
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById('modelBadge').innerHTML = `🤖 AI Model: ${data.model}`;
-    });
